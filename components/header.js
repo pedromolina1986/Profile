e61279b6dfb900e779e5a1eea964ba1dd29a2c58
phkmolina1986 <pedro.kuchak@gmail.com>
2025-03-12 20:16:23 -0600

    menuLeft

--- components/header.js
+++ components/header.js
@@ -1,17 +1,24 @@
 class Header extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
-        <div class="header">
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+        <div class="header">           
             <div>
-                <div class="bars">
+                <div class="bars" onClick="openMenuLeft()">
                     <div></div>
-                     <div></div>
                     <div></div>
-                </div>
+                    <div></div>                                        
+                </div>                
+            </div>
+            <div id="menuLeft" class="menuLeft">
+              <span><a href="/pedromolina.github.io/index.html">HOME</a></span>
+              <span><a href="/pedromolina.github.io/pages/about.html">ABOUT</a></span>
+              <span><a href="/pedromolina.github.io/pages/portfolio.html">PORTFOLIO</a></span>
+              <span><a href="/pedromolina.github.io/pages/blog.html">BLOG</a></span>
+              <span><a href="/pedromolina.github.io/pages/contact.html">CONTACT</a></span>
             </div>
             <div class="logo">
                 <span>myProfile.</span>
@@ -26,7 +33,19 @@ class Header extends HTMLElement {
 
         </div>
       `;
-    }
   }
-  
-  customElements.define('header-component', Header);
+}
+
+function openMenuLeft() {
+  const menuLeft = document.getElementById('menuLeft')
+  // get all rendered styles
+  const styles = window.getComputedStyle(menuLeft)
+  if (styles.visibility == "hidden") {
+    document.getElementById("menuLeft").style.visibility = "visible";
+  }
+  else {
+    document.getElementById("menuLeft").style.visibility = "hidden";
+  }
+}
+
+customElements.define('header-component', Header);

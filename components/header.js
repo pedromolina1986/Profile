8e5c2cb7269f510c2db4871bf749a2a4552e0952
phkmolina1986 <pedro.kuchak@gmail.com>
2025-07-05 20:15:43 -0600

    menu fix

--- components/header.js
+++ components/header.js
@@ -18,21 +18,21 @@ class Header extends HTMLElement {
                 </div>                
             </div>
             <div id="menuLeft" class="menuLeft">
-              <span><a href="/pedromolina.github.io/index.html">home</a></span>
-              <span><a href="/pedromolina.github.io/pages/about.html">about</a></span>
-              <span><a href="/pedromolina.github.io/pages/portfolio.html">tutorials</a></span>
-              <span><a href="/pedromolina.github.io/pages/blog.html">blog</a></span>
-              <span><a href="/pedromolina.github.io/pages/contact.html">contact</a></span>
+              <span><a href="/Profile/index.html">home</a></span>
+              <span><a href="/Profile/pages/about.html">about</a></span>
+              <span><a href="/Profile/pages/portfolio.html">tutorials</a></span>
+              <span><a href="/Profile/pages/blog.html">blog</a></span>
+              <span><a href="/Profile/pages/contact.html">contact</a></span>
             </div>
             <div class="logo">
                 <span>myProfile.</span>
             </div>
            <div id="menu" class="menu">
-              <span><a href="/pedromolina.github.io/index.html">home</a></span>
-              <span><a href="/pedromolina.github.io/pages/about.html">about</a></span>
-              <span><a href="/pedromolina.github.io/pages/portfolio.html">tutorials</a></span>
-              <span><a href="/pedromolina.github.io/pages/blog.html">blog</a></span>
-              <span><a href="/pedromolina.github.io/pages/contact.html">contact</a></span>
+              <span><a href="/Profile/index.html">home</a></span>
+              <span><a href="/Profile/pages/about.html">about</a></span>
+              <span><a href="/Profile/pages/portfolio.html">tutorials</a></span>
+              <span><a href="/Profile/pages/blog.html">blog</a></span>
+              <span><a href="/Profile/pages/contact.html">contact</a></span>
             </div>
         </div>
       `;
